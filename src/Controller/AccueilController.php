e5fd1bf8928feeb760dbaaa4ac2bd0bd85317ff2
0livierD <olivier.dy@gmail.com>
2024-04-21 20:43:17 +0200

    la base

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('accueil/index.html.twig');
     }
