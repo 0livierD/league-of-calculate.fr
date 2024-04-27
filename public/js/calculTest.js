872537dd0a31ad6a5b2e8176fdf23e004edb2816
0livierD <olivier.dy@gmail.com>
2024-04-27 17:10:08 +0200

    3 niveaux solo

--- public/js/calculTest.js
+++ public/js/calculTest.js
@@ -1,50 +1,98 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let operation = document.querySelector('#operation')
-    let reponse = document.querySelector('#reponse')
-    let infoResultat = document.querySelector('#infoResultat')
+        let operation = document.querySelector('#operation')
+        let reponse = document.querySelector('.reponse')
+        let infoResultat = document.querySelector('#infoResultat')
+        let affichageScore = document.querySelector('#affichageScore')
+        let affichageNiveau = document.querySelector('#affichageNiveau')
+        let niveauSuivant = document.querySelector('.niveauSuivant')
 
-    let score = 0
+        let niveau = 1
+        affichageNiveau.innerHTML = 'Niveau ' + niveau
 
-    function lancerQuestion() {
+        let score = 0
+        let number
+        let number2
+        let resultat
+        lancerQuestion()
 
-        let number = Math.floor(Math.random() * 10)
-        let number2 = Math.floor(Math.random() * 10)
+        function lancerQuestion() {
 
-        let resultat = number + number2
+            affichageScore.innerHTML = score + ' / 5'
 
-        operation.innerHTML = number + ' + ' + number2
+            switch (niveau) {
+                case 1:
+                    number = Math.floor(Math.random() * 10)
+                    number2 = Math.floor(Math.random() * 10)
+                    resultat = number + number2
+                    operation.innerHTML = number + ' + ' + number2
+                    break
 
-        function saisie(e) {
-            if (e.key === 'Backspace') {
-                if (reponse != null) {
-                    reponse.innerHTML = reponse.innerHTML.substring(0, (reponse.innerHTML.length - 1))
-                }
-            } else if (e.key === 'Enter') {
-                if (parseInt(reponse.innerHTML) === resultat) {
-                    infoResultat.className = "fa-solid fa-thumbs-up victoire"
-                    score++
+                case 2 :
+                    number = Math.floor(Math.random() * 100)
+                    number2 = Math.floor(Math.random() * 10)
+                    resultat = number + number2
+                    operation.innerHTML = number + ' + ' + number2
+                    break
+
+                case 3 :
+                    number = Math.floor(Math.random() * 10)
+                    number2 = Math.floor(Math.random() * 10)
+                    if (number < number2) {
+                        resultat = number2 - number
+                        operation.innerHTML = number2 + ' - ' + number
+                    } else {
+                        resultat = number - number2
+                        operation.innerHTML = number + ' - ' + number2
+                    }
+                    break
+            }
+
+            document.addEventListener('keydown', saisie)
+
+            function saisie(e) {
+                if (e.key === 'Backspace') {
+                    if (reponse != null) {
+                        reponse.innerHTML = reponse.innerHTML.substring(0, (reponse.innerHTML.length - 1))
+                    }
+                } else if (e.key === 'Enter') {
+                    if (parseInt(reponse.innerHTML) === resultat) {
+                        infoResultat.className = "fa-solid fa-thumbs-up victoire"
+                        score++
+                    } else {
+                        infoResultat.className = "fa-solid fa-thumbs-down defaite"
+                    }
+
+                    reponse.innerHTML = ""
+                    document.removeEventListener('keydown', saisie)
+
+                    if (score === 5) {
+                        affichageScore.innerHTML = score + ' / 5'
+                        reponse.className = "bravo"
+                        reponse.innerHTML = "Bravo, niveau " + niveau + " réussi !"
+                        niveau++
+                        niveauSuivant.classList.remove('displayNone')
+                        niveauSuivant.addEventListener('click', test)
+                    } else {
+                        lancerQuestion()
+                    }
                 } else {
-                    infoResultat.className = "fa-solid fa-thumbs-down defaite"
-                }
-                reponse.innerHTML = ""
-                document.removeEventListener('keydown', saisie)
-                lancerQuestion()
-
-            } else if (score === 5) {
-                console.log('gagné')
-                document.removeEventListener('keydown', saisie)
-            } else {
-                const num = parseInt(e.key)
-                if (num >= 0 && num <= 9) {
+                    const num = parseInt(e.key)
+                    if (num >= 0 && num <= 9) {
+                        reponse.innerHTML = reponse.innerHTML + num
+                    }
                 }
             }
+
+            function test() {
+                score = 0
+                niveauSuivant.classList.add('displayNone')
+                reponse.className = "reponse"
+                reponse.innerHTML = ""
+                affichageNiveau.innerHTML = 'Niveau ' + niveau
+                niveauSuivant.removeEventListener('click', test)
+                lancerQuestion()
+            }
         }
-
-        document.addEventListener('keydown', saisie)
     }
-
-    lancerQuestion()
-
-
-})
+)
