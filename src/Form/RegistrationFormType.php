d895d7f22bf07ed6e1bdd3d28ff6d4036500c027
0livierD <olivier.dy@gmail.com>
2024-05-20 11:13:09 +0200

    refonte

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
+use phpDocumentor\Reflection\PseudoTypes\Numeric_;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
