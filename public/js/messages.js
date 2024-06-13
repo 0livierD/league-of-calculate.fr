bae43f70dbe7fad9788016585a48129a52239873
0livierD <olivier.dy@gmail.com>
2024-06-13 10:35:53 +0200

    ajout message d'erreur sur login

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,23 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let erreurs = document.querySelectorAll("div#erreurs > ul > li")
+    let erreurLogin = document.querySelector('#erreurLogin')
     let divErreur = document.querySelector("#divErreur")
     let instit = document.querySelector('.instit')
     let bulle = document.querySelector('.bulle')
 
-    if (erreurs[0] != null) {
+    if (erreurs[0] != null || erreurLogin.innerHTML.length !== 22) {
         instit.classList.remove('displayNone')
         bulle.classList.remove('displayNone')
         let message = document.createElement('div')
         message.classList.add('erreur')
-        erreurs.forEach(e => {
-            if (e.innerHTML === 'Il y a déjà un compte avec cet email !') {
-                message.innerHTML = e.innerHTML
+
+        if (erreurs[0] != null) {
+            erreurs.forEach(e => {
+                if (e.innerHTML === 'Il y a déjà un compte avec cet email !') {
+                    message.innerHTML = e.innerHTML
+                }
+            })
+            if (message.innerHTML !== 'Il y a déjà un compte avec cet email !') {
+                message.innerHTML = erreurs[0].innerHTML
             }
-        })
-        if (message.innerHTML !== 'Il y a déjà un compte avec cet email !') {
-            message.innerHTML = erreurs[0].innerHTML
+        } else {
+            message.innerHTML = 'Email ou mot de passe incorrect !!'
         }
+
         divErreur.appendChild(message)
 
         setTimeout( () => {
